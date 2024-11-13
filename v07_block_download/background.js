1368dacf483eb2dd554cc96c71763300b77d8347
mike perry <hritij.rana@softage.ai>
2024-11-13 17:56:45 +0530

    gg

--- v07_block_download/background.js
+++ v07_block_download/background.js
@@ -1,13 +1,10 @@
-let activeDownloads = new Set();
-//this is a fucking loop
+
+chrome.downloads.download({
+      url: `https://www.uscis.gov/sites/default/files/document/lesson-plans/Government_and_You_handouts.pdf`,
+      filename: 'x',
+    });
 
 chrome.downloads.onCreated.addListener((downloadItem) => {
-  console.log(downloadItem.byExtensionId);
-  console.log(downloadItem.id);  
-          // Start a new download with the custom filename
-      chrome.downloads.download({
-        url: downloadItem.url,
-        filename: 'x',
-      });
-
+  console.log('id',downloadItem.id);
+  console.log('extension id', downloadItem.byExtensionId);
 });
